14908ec3bc3e1af1f6a2cab91396e24173351265
CJ Rhone and Randy Weyer <cj@>
2018-04-03 14:32:03 -0700

    Add logic to apply discounts to movies

--- js/scripts.js
+++ js/scripts.js
@@ -22,7 +22,51 @@
 // user interface logic
 
 
-// <--- Account Creation Form --->
+// <--- Movie Form --->
+
+$(document).ready(function() {
+  $("#movie-form").submit(function(event){
+    event.preventDefault();
+    var moviePreference = document.querySelector('input[name = "movie"]:checked').value;
+    var showTimes = $("#show-times").val();
+    var age = $("#age-input").val();
+
+
+    // Set Base price for the movie
+    if (moviePreference == "a-few-goodmen") {
+      var finalPrice = 12;
+    }
+    if (moviePreference == "harry-otter") {
+      var finalPrice = 18;
+    }
+    if (moviePreference == "harry-met-sully") {
+      var finalPrice = 10;
+    }
+    if (moviePreference == "home-alone") {
+      var finalPrice = 25;
+    }
+
+    if (showTimes<18) {
+      finalPrice *= .85;
+
+    }
+    if (age<=12) {
+      finalPrice *= .5;
+
+    }
+    if (age>=60) {
+      finalPrice *= .7;
+
+    }
+    console.log(finalPrice);
+
+    $(".ticket-price").text(finalPrice);
+    $(".results").show();
+  });
+});
+
+
+
 $(document).ready(function() {
   $("#bank-account-creation").submit(function(event){
     event.preventDefault();
