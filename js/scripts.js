8111b2a11623ecc0d2ea75df0f8fed1cc7146116
CJ Rhone and Randy Weyer <cj@>
2018-04-03 15:00:16 -0700

    update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,5 @@
 // business logic
 
-// var newAccount;
-//
-// function Account(name, balance) {
-//   this.name = name;
-//   this.balance = balance;
-// }
-//
-// Account.prototype.deposit = function(amount) {
-//   return this.balance += amount;
-// }
-//
-// Account.prototype.withdraw = function(amount) {
-//   return this.balance -= amount;
-// }
-
-
-
-
 
 // user interface logic
 
